0ac91852c26c96d7f2e61ce487d4d7d9bf791384
quyet <=>
2022-11-16 00:20:53 +0700

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'chart';
+export class AppComponent implements OnInit {
+  title = 'chartDemo';
+  ngOnInit()
+  {
+    var myChart = new Chart("myChart", {
+      type: 'line',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: 'Data1',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor:"#0196FD",
+          borderColor: "#0196FD",
+          borderWidth: 1
+        },
+          {
+            label: 'Dat21',
+            data: [19, 12, 5, 3, 1, 6],
+            backgroundColor:"#FFAF00",
+            borderColor: "#FFAF00",
+            borderWidth: 1
+          }]
+      },
+      options: {
+        scales: {
+          // y: {
+          //   beginAtZero: true
+          // }
+        }
+      }
+    });
+  }
 }
